7c0722310a9f94e5c7fd1b564c652747d1d3acd6
Deepeshgiri <dr.deepeshgiri@gmail.com>
2024-04-01 20:40:56 +0530

    bkank msg

---
+++ src/components/messages/StartMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function StartMessage() {
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <div className="bg-slate-600 px-4 ">
+        
+      </div>
+<h1>Select any chat to start messaging!!  </h1>
+      
+    </div>
+  )
+}
+
+export default StartMessage
